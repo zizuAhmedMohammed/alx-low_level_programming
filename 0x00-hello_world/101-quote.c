4c2b40910304eae6fd5641325538df1f274bcd30
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-02 17:38:17 -0700

    Define quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - Running of execution
  *
- * Return: Always 0 (Success)
+ * Return: 1
  */
 int main(void)
-{
-	fwrite(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+{	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, 59);
 	return (1);
 }
