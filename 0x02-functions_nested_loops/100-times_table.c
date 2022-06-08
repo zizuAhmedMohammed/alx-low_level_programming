fc7738505fefff2c5e667a1cfc5bc01aeb1900ba
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-07 19:49:44 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,10 @@
-include "main.h"
+#include "main.h"
 
 /**
  * print_times_table - prints the times table
  * @n: integer for which the times table will be printed
  *
- * Description: prints the time table
+ * Description: prints the times table
  *
  * Return: void
  */
