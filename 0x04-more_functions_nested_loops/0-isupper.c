1d8ab9f02ad0cfdd31c1d208d37632ca2a984723
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-09 12:09:05 -0700

    Modify 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - check for uppercase character
- * @x: the number to be checked
+ * @c: the number to be checked
  *
  * Return: 1 for uppercase character or 0 for any other
  */
 
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
