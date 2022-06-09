5218bb1b8598cfbc246d3c1d231dfc2b7d930db1
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-09 13:16:26 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line according to parameter
+ * @n: The number of lines to draw
+ *
+ * Return: underscore or nothing
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
+}
