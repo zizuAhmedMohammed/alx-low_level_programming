e7f918853433a9ddcf5b52d0d75eff7f2b570542
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-13 15:33:30 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in stdout in reverse
+ * @s: string to print
+ *
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+
+	_putchar('\n');
+}
