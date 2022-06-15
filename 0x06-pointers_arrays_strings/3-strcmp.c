b3a67695644ae6f83310224800569993443a7a0e
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-15 13:48:38 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.c"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: int value
+ */
+
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, b = 0, c = 0, v = 0, lim;
+
+	while (s1[a])
+	{
+		a++;
+	}
+
+	return (v);
+}
