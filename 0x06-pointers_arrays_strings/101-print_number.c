49e3794963a73155064e8eac773363f163d9bf48
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-16 10:01:00 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - Print an integer
+ * @n: An input integer number
+ *
+ * Return: Integer value
+ */
+
+void print_number(int n)
+{
+	int n;
+
+	_putchar("%d", n);
+
+	return (0);
+}
