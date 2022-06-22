d18bb3f54c8366b7b0a03ddbae40703ac817649a
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-22 14:30:04 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,6 +9,7 @@
  *
  * Return: spot that matches or NULL
  */
+
 char *test(char *s5, char *s6, char *t5, char *t6)
 {
 	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
@@ -18,6 +19,7 @@ char *test(char *s5, char *s6, char *t5, char *t6)
 	else
 		return (test(s5 + 1, s6 + 1, t5, t6));
 }
+
 /**
  * wildcard - checks for the wildcards
  * @s3: string to compare to
@@ -25,6 +27,7 @@ char *test(char *s5, char *s6, char *t5, char *t6)
  *
  * Return: pointer to spot in s3 that matches s4
  */
+
 char *wildcard(char *s3, char *s4)
 {
 	if (*s3 != *s4)
@@ -43,6 +46,7 @@ char *wildcard(char *s3, char *s4)
  *
  * Return: 1 if match, 0 if not
  */
+
 int wildcmp(char *s1, char *s2)
 {
 	char *p;
@@ -62,5 +66,6 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+
 	return (wildcmp(s1 + 1, s2 + 1));
 }
