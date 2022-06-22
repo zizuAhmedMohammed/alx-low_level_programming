6b1126c836be1a5ceda06d6d8b21fa01120ccc76
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-22 10:56:34 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,11 +11,11 @@ void _puts_recursion(char *s)
 {
 	if (*s == '/0')
 	{
-		-putchar('\n');
+		_putchar('\n');
 		return;
 	}
 
 	_putchar(*s);
-	*s++;
-	_puts_recursion(*s);
+	s++;
+	_puts_recursion(s);
 }
