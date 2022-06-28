943ac7c3f34328c7b30315f421ed87f19499e664
Ziz Ahmed <azizazu0934@gmail.com>
2022-06-28 12:50:43 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,7 +28,25 @@ char *str_concat(char *s1, char *s2)
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 
-	s[l] = '\0';
+	if (s == NULL)
+		return (NULL);
+
+	j = 0;
+
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+
+		k++;
+	}
+
+	s[k] = '\0';
 	return (s);
 }
-	
