df301ef0b154d20d2f381969efb0956520e10189
Ziz Ahmed <azizazu0934@gmail.com>
2022-07-04 12:05:24 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Prints the file name of the program
+ *
+ * Return: Always 0
+ */
 int main()
 {
 	printf("File: %s\n", __FILE__);
+	return (0);
 }
