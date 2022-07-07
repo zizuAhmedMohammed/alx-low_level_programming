9f2f61e61fccf9c95653d0cac637b5a78197da76
Ziz Ahmed <azizazu0934@gmail.com>
2022-07-07 14:54:58 -0700

    Modify header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,13 +2,6 @@
 #define _VARIADIC_FUNCTIONS_H_
 
 #include <stdarg.h>
-
-int _putchar(char);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-
 /**
  * struct print - print type with corresponding print function
  * @t: print type
@@ -20,4 +13,10 @@ typedef struct print
 	void (*f)(va_list);
 } print_t;
 
-#endif
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
